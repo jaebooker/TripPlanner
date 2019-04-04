2935a27a15a32e51ab126a95e38b4540152f01ab
jaebooker <jaesonbooker@gmail.com>
2019-04-03 19:59:11 -0700

    added core data model

--- TripPlanner/ViewController.swift
+++ TripPlanner/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let trips: [String] = []
+    
+    let trips = [TripListItem]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
